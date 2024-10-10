6d1a6ab05b8ab96c9feb74ba3e375bdf560b9405
alexflomboim <64857239+alexflomboim@users.noreply.github.com>
2024-10-10 18:17:54 +0300

    Add attributes and Health progress widget

---
+++ Source/MyGame/VitalAttributeSet.h
@@ -0,0 +1,31 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "TPAttributeSet.h"
+#include "VitalAttributeSet.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MYGAME_API UVitalAttributeSet : public UTPAttributeSet
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(BlueprintReadOnly)
+	FGameplayAttributeData Health;
+	ATTRIBUTE_ACCESSORS(UVitalAttributeSet, Health)
+
+	UPROPERTY(BlueprintReadOnly)
+	FGameplayAttributeData MaxHealth;
+	ATTRIBUTE_ACCESSORS(UVitalAttributeSet, MaxHealth)
+
+	UPROPERTY(BlueprintReadOnly)
+	FGameplayAttributeData Hunger;
+	ATTRIBUTE_ACCESSORS(UVitalAttributeSet, Hunger)
+
+	
+};
