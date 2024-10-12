c3cc0c37f9f538b235bbcb3416513177e53bd25c
alexflomboim <64857239+alexflomboim@users.noreply.github.com>
2024-10-12 21:06:44 +0300

    Task 24 update, GetAvatarActor

--- Source/MyGame/MovementAttributeSet.cpp
+++ Source/MyGame/MovementAttributeSet.cpp
@@ -8,12 +8,14 @@
 
 void UMovementAttributeSet::PostAttributeChange(const FGameplayAttribute& Attribute, float OldValue, float NewValue)
 {
-	if (Attribute == GetSpeedAttribute()) {
-		ATPPlayerState *PlayerState = Cast<ATPPlayerState>(GetOwningActor());
+	if (Attribute == GetSpeedAttribute()) {	
+		UCharacterMovementComponent* MovementPtr = Cast<AMyGameCharacter>(GetOwningAbilitySystemComponent()->GetAvatarActor())->GetCharacterMovement();
+		MovementPtr->MaxWalkSpeed = NewValue;
+		/*ATPPlayerState* PlayerState = Cast<ATPPlayerState>(GetOwningActor());
 		if (PlayerState != nullptr) {
 			AMyGameCharacter *character = Cast<AMyGameCharacter>(PlayerState->GetPawn());
 			UCharacterMovementComponent* MovementPtr = character->GetCharacterMovement();
 			MovementPtr->MaxWalkSpeed = NewValue;
-		}
+		}*/
 	}
 }
